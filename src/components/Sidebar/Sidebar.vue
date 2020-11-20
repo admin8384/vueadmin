8e6a36df83b9860ba48eb73b0819428ef2260407
isxxx <838495500@qq.com>
2020-11-20 09:01:15 +0800

    first comit

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="sidebar-wrapper">
+    <nav
+        :class="{sidebar: true, sidebarStatic, sidebarOpened}"
+        @mouseenter="sidebarMouseEnter"
+        @mouseleave="sidebarMouseLeave"
+    >
+      <header class="logo">
+        <router-link to="/app"><span class="primary-word">云票儿</span> 管理平台</router-link>
+      </header>
+      <ul class="nav">
+        <NavLink
+            :activeItem="activeItem"
+            header="仪表盘"
+            link="/app/dashboard"
+            iconName="flaticon-home"
+            index="dashboard"
+            isHeader
+        />
+        <NavLink
+            :activeItem="activeItem"
+            header="字体样式"
+            link="/app/typography"
+            iconName="flaticon-list"
+            index="typography"
+            isHeader
+        />
+        <NavLink
+            :activeItem="activeItem"
+            header="表格"
+            link="/app/tables"
+            iconName="flaticon-equal-1"
+            index="tables"
+            isHeader
+        />
+        <NavLink
+            :activeItem="activeItem"
+            header="组件"
+            link="/app/components"
+            iconName="flaticon-network"
+            index="components"
+            :childrenLinks="[
+              { header: '图表', link: '/app/components/charts' },
+              { header: '图标', link: '/app/components/icons' },
+              { header: '地图', link: '/app/components/maps' },
+            ]"
+        />
+      </ul>
+   
+      
+      
+    </nav>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import isScreen from '@/core/screenHelper';
+import NavLink from './NavLink/NavLink';
+
+export default {
+  name: 'Sidebar',
+  components: { NavLink },
+  data() {
+    return {
+      alerts: [
+        {
+          id: 0,
+          title: 'Sales Report',
+          value: 15,
+          footer: 'Calculating x-axis bias... 65%',
+          color: 'danger',
+        },
+        {
+          id: 1,
+          title: 'Personal Responsibility',
+          value: 20,
+          footer: 'Provide required notes',
+          color: 'primary',
+        },
+      ],
+    };
+  },
+  methods: {
+    ...mapActions('layout', ['changeSidebarActive', 'switchSidebar']),
+    setActiveByRoute() {
+      const paths = this.$route.fullPath.split('/');
+      paths.pop();
+      this.changeSidebarActive(paths.join('/'));
+    },
+    sidebarMouseEnter() {
+      if (!this.sidebarStatic && (isScreen('lg') || isScreen('xl'))) {
+        this.switchSidebar(false);
+        this.setActiveByRoute();
+      }
+    },
+    sidebarMouseLeave() {
+      if (!this.sidebarStatic && (isScreen('lg') || isScreen('xl'))) {
+        this.switchSidebar(true);
+        this.changeSidebarActive(null);
+      }
+    },
+  },
+  created() {
+    this.setActiveByRoute();
+  },
+  computed: {
+    ...mapState('layout', {
+      sidebarStatic: state => state.sidebarStatic,
+      sidebarOpened: state => !state.sidebarClose,
+      activeItem: state => state.sidebarActiveElement,
+    }),
+  },
+};
+</script>
+
+<!-- Sidebar styles should be scoped -->
+<style src="./Sidebar.scss" lang="scss" scoped/>
